b6887014ccf35caf84691d94a6986220349778b2
Mohd irshad <66816040+MohdIrshad9473@users.noreply.github.com>
2021-07-25 00:51:00 +0530

    init

---
+++ react-shopping-card/src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className="wrapper">
+    <header>
+      <a href="/">React shopping Card</a>
+    </header>
+    <main>I'm the main-content filling the void!</main>
+    <footer>All Right is reserved</footer>
+</div>
+  );
+}
+
+export default App;
